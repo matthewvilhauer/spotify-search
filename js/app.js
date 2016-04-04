eb1450b01ee1cae9970f817f8023a6f4e3c7a50d
Matthew Vilhauer <matthewvilhauer@gmail.com>
2016-04-04 01:54:06 -0700

    Added GitHub API search form that grabs and displays all of the repositories for the searched user

--- js/app.js
+++ js/app.js
@@ -1,5 +1,23 @@
 // wait for DOM to load before running JS
+
+
 $(document).on('ready', function() {
+
+  var source = $('#tracks-template').html();
+  var template = Handlebars.compile(source);
+
+  $(".loading").hide();
+
+  $(document).ajaxStart(function() {
+
+    $(".loading").show();
+    $("#results").hide();
+    console.log("Ajax started");
+  }).ajaxStop(function() {
+    $(".loading").hide();
+    $("#results").show();
+    console.log("Ajax stopped");
+  });
 
   // check to make sure JS is loaded
   console.log('JS is loaded!');
@@ -12,36 +30,38 @@ $(document).on('ready', function() {
     // var typeQuery = "&type=track";
     // var dataString = searchQuery + typeQuery;
 
-    var response = $.ajax({
+    $.ajax({
       method: "GET",
       url: spotify_endpoint,
       data: $("form").serialize(),
       success: onSuccess,
       error: onError
     });
-
   }
 
-  // function getArtists(artists) {
-  //   artists.forEach(function(element) {
-  //     return element.name;
-  //   });
-  // }
+  function onSuccess(data) {
 
-  function onSuccess(response) {
-    console.log(response);
-    $("#results").text("");
-    $("#searchResults").append("<h2>Results for: " + $("#track").val() + "</h2>");
-    response.tracks.items.forEach(function(element) {
-      $("#results").append("<div class='container'><div class='row'><div class='col-sm-3 albumArt'><img src=" + element.album.images[1].url + "></div><div class=col-sm-9><div class='col-sm-3 song'><p>Song: "+element.name+"</p></div><div class='col-sm-3 album'><p>Album: "+element.album.name + "</p></div><div class='col-sm-3 artist'><p>Artist: " + element.artists[0].name + "</p></div><div class='col-sm-3 play'><a href=" + element.preview_url + ">Play</a></div></div></div>");
-    });
-  }
+      $("#results").text("");
+      var trackResults = data.tracks.items;
+      console.log(trackResults);
+      var trackHtml = template({
+        tracks: trackResults
+      });
+      // $("#searchResults").append("<h2>Results for: " + $("#track").val() + "</h2>");
+      // trackResults.forEach(function(el) {
+      //   $("#results").append("<div class='container'><div class='row'><div class='col-sm-3 albumArt'><img src=" + el.album.images[1].url + "></div><div class=col-sm-9><div class='col-sm-3 song'><p>Song: "+el.name+"</p></div><div class='col-sm-3 album'><p>Album: "+el.album.name + "</p></div><div class='col-sm-3 artist'><p>Artist: " + el.artists[0].name + "</p></div><div class='col-sm-3 play'><a href=" + el.preview_url + ">Play</a></div></div></div>");
+      // });
+      $("#results").append(trackHtml);
+    }
 
   var search = $("form").on("submit", function(e) {
+    if($("#results").text() !== "") {
       e.preventDefault();
       getTracks();
+    } else {
+      alert("No Results");
+    }
   });
-
 
   function onError(xhr, status, errorThrown) {
     alert("Sorry, there was a problem!");
